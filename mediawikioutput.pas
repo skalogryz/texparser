b071aec70e484ddcfa27c7da98d6180062d09de8
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2022-06-02 13:10:40 -0400

    writing out the plain text

--- mediawikioutput.pas
+++ mediawikioutput.pas
@@ -23,6 +23,7 @@ var
   txt : string;
   f   : TOutputFile;
   ownopt : Boolean;
+  t : TTexEntity;
 begin
   e := nil;
   ownopt := not Assigned(opt);
@@ -44,6 +45,7 @@ begin
         f := ot.AddFile(e.ArgText(0));
         //writeln('=', e.ArgText(0) ,'=');
       end else if (cmd = 'section') then begin
+        f.WrLn;
         f.Wr('==');
         f.Wr(e.ArgText(0));
         f.WrLn('==');
@@ -51,6 +53,21 @@ begin
         f.Wr('===');
         f.Wr(e.ArgText(0));
         f.WrLn('===');
+      end else if (cmd = 'begin') and (e.ArgText(0)='verbatim') then begin
+        f.Wr('<code>');
+        f.Wr(e.OptText(0));
+        f.WrLn('</code>');
+      end else if cmd<>'' then begin
+        f.Wr('@@');
+        f.Wr(cmd);
+        f.Wr('@@');
+      end else if e.entType = tetText then begin
+        t := e;
+        while Assigned(t) do begin
+          if (t.entType = tetText) then f.Wr(t.text);
+          t := t.next;
+        end;
+        f.WrLn;
       end;
       //end;
     until not Assigned(e);
